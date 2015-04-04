aede45909e780a484ebf5a0c03fee14b89eace99
Bohdan Korniyenko <bohdan.korniyenko@gmail.com>
2015-04-05 00:51:03 +0300

    2

--- my_rest/urls.py
+++ my_rest/urls.py
@@ -22,6 +22,6 @@ urlpatterns = patterns('',
 #    url(r'api/v1/auth/login/', 'rest_framework_jwt.views.obtain_jwt_token'),
 #    url(r'api/v1/users/', UserListAPIView.as_view()),
 
-    url(r'^.*$', TemplateView.as_view(template_name='index.html')),
+#    url(r'^.*$', TemplateView.as_view(template_name='index.html')),
 
 )
